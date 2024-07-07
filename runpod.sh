1ad607b0c77f2b53236fcb47a3e452d5428699f1
qwertyuiop1923 <56924835+qwertyuiop1923@users.noreply.github.com>
2024-07-07 23:08:22 +0900

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xd325710644cC3dFdfE459e31B7202c33Af06CEf4/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
